d0a20b010eb68817bfcb2baea7b0e9d5d84183ab
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-05-27 09:54:17 +0300

    Tasks from course PostgreSQL

--- PostgreSQL_Data_Aggregation_Exercise/10.wizard_view.sql
+++ PostgreSQL_Data_Aggregation_Exercise/10.wizard_view.sql
@@ -4,7 +4,14 @@ SELECT
     deposit_start_date AS start_date,
     deposit_expiration_date AS expiration_date,
     deposit_amount AS amount
-FROM wizard_deposits
-WHERE expiration_date >= '1983-08-17'
-GROUP BY wizard_name, start_date, expiration_date, amount
-ORDER BY expiration_date
+FROM 
+    wizard_deposits
+WHERE 
+    deposit_expiration_date <= '1983-08-17'
+GROUP BY 
+    wizard_name, 
+    start_date, 
+    expiration_date, 
+    amount
+ORDER BY
+    expiration_date ASC
