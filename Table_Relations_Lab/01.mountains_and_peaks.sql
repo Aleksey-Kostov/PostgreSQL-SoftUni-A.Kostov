d04dbf87a8aac7abcc1c03e714f0f8d72716d721
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-05-28 13:29:48 +0300

    Tasks from course PostgreSQL

--- Table_Relations_Lab/01.mountains_and_peaks.sql
+++ Table_Relations_Lab/01.mountains_and_peaks.sql
@@ -6,10 +6,13 @@ CREATE TABLE mountains(
 CREATE TABLE peaks(
     id serial PRIMARY KEY,
     name varchar(50),
-    mountain_id int
+    mountain_id int, --REFERENCES mountains(id)
+    CONSTRAINT fk_peaks_mountains
+    FOREIGN KEY (mountain_id)
+          REFERENCES mountains(id)
 );
 
-ALTER TABLE peaks
-ADD CONSTRAINT fk_peaks_mountains
-    FOREIGN KEY (mountain_id)
-        REFERENCES mountains(id);
+-- ALTER TABLE peaks
+-- ADD CONSTRAINT fk_peaks_mountains
+--     FOREIGN KEY (mountain_id)
+--         REFERENCES mountains(id);
