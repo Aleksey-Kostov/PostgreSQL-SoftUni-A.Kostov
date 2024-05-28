fbbd76f23f7f995de8cebe15b2c24f43bb625716
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-05-28 12:50:40 +0300

    Tasks from course PostgreSQL

---
+++ Table_Relations_Lab/01.mountains_and_peaks.sql
@@ -0,0 +1,15 @@
+CREATE TABLE mountains(
+    id serial PRIMARY KEY,
+    name varchar(50)
+);
+
+CREATE TABLE peaks(
+    id serial PRIMARY KEY,
+    name varchar(50),
+    mountain_id int
+);
+
+ALTER TABLE peaks
+ADD CONSTRAINT fk_peaks_mountains
+    FOREIGN KEY (mountain_id)
+        REFERENCES mountains(id);
