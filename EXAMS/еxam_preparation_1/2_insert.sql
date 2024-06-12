89457509064bde101e60572ebefe2b4d9fa0dbe8
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-06-12 09:38:57 +0300

    Tasks from course PostgreSQL

--- EXAMS/еxam_preparation_1/2_insert.sql
+++ EXAMS/еxam_preparation_1/2_insert.sql
@@ -1,10 +1,10 @@
-INSERT INTO volunteers("name", phone_bumber, "address", animal_id, department_id)
+INSERT INTO volunteers("name", phone_number, "address", animal_id, department_id)
 VALUES
-("Anita Kostova", '0896365412', "Sofia, 5 Rosa str.", 15, 1),
-("Dimitur Stoev", '0877564223', NULL, 42, 4),
-("Kalina Evtimova", '0896321112', "Silistra, 21 Breza str.", 9, 7),
-("Stoyan Tomov", '0898564100', "Montana, 1 Bor str.", 18, 8),
-("Boryana Mileva", '0888112233', NULL, 31, 5);
+('Anita Kostova', '0896365412', 'Sofia, 5 Rosa str.', 15, 1),
+('Dimitur Stoev', '0877564223', NULL, 42, 4),
+('Kalina Evtimova', '0896321112', 'Silistra, 21 Breza str.', 9, 7),
+('Stoyan Tomov', '0898564100', 'Montana, 1 Bor str.', 18, 8),
+('Boryana Mileva', '0888112233', NULL, 31, 5);
 
 INSERT INTO animals("name", birthdate, owner_id, animal_type_id)
 VALUES
