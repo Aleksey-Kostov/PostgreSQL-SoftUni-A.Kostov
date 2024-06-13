8ce2f4e43c68cb319149bc9b195db03c9153ebae
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-06-13 14:17:58 +0300

    Tasks from course PostgreSQL

--- EXAMS/еxam_preparation_2/7_number_of_courses_for_each_car.sql
+++ EXAMS/еxam_preparation_2/7_number_of_courses_for_each_car.sql
@@ -1,12 +1,17 @@
 SELECT 
 c.id AS car_id,
 c.make AS make,
+c.mileage AS mileage,
 count(co.client_id) AS count_of_courses,
-AVG(co.bill) AS average_bill
+ROUND((AVG(co.bill)), 2) AS average_bill
 FROM cars AS c
-LEFT JOIN courses AS co 
-ON co.car_id = c.id
-JOIN clients AS cl
-ON cl.id = co.client_id
-GROUP BY c.id, c.make
-ORDER BY count_of_courses DESC, car_id
+LEFT JOIN 
+	courses AS co 
+    ON co.car_id = c.id
+GROUP BY 
+	c.id, c.make, c.mileage
+HAVING 
+	count(co.id) <> 2
+ORDER BY 
+	count_of_courses DESC, 
+	car_id
