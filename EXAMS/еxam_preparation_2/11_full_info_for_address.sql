054647b0cebc20565206651b0e7531aa474705fe
Aleksey Kostov <aleksey_kostov@abv.bg>
2024-06-13 19:26:06 +0300

    Tasks from course PostgreSQL

--- EXAMS/еxam_preparation_2/11_full_info_for_address.sql
+++ EXAMS/еxam_preparation_2/11_full_info_for_address.sql
@@ -1,21 +1,67 @@
-CREATE OR REPLACE PROCEDURE ssp_courses_by_address() (
-	IN address_name VARCHAR(100),
-	OUT address_name VARCHAR(50),
-    OUT full_name VARCHAR(100),
-    OUT level_of_bill VARCHAR(20),
-    OUT make VARCHAR(30),
-    OUT condition CHAR(1),
-    OUT category_name VARCHAR(50)
-)
+CREATE TABLE search_results (
+    id SERIAL PRIMARY KEY,
+    address_name VARCHAR(50),
+    full_name VARCHAR(100),
+    level_of_bill VARCHAR(20),
+    make VARCHAR(30),
+    condition CHAR(1),
+    category_name VARCHAR(50)
+);
+
+
+
+
+CREATE OR REPLACE PROCEDURE sp_courses_by_address(
+	address_name VARCHAR(100))
 AS 
 $$
 BEGIN
-	UPDATE
-		accounts
-	SET 
-		balance = balance + money_amount
+    TRUNCATE TABLE search_results;
+	
+	INSERT INTO search_results(
+		address_name,
+		full_name,
+		level_of_bill,
+		make,
+		condition,
+		category_name
+	)
+
+	SELECT 
+	a.name AS address_name,
+	cl.full_name AS full_name,
+	CASE
+	WHEN co.bill <= 20 THEN 'Low'
+	WHEN co.bill <= 30 THEN 'Medium'
+	ELSE 'High'
+	END AS level_of_bill,
+	c.make AS make,
+	c.condition AS condition,
+	ca.name AS category_name 
+	FROM "addresses" AS a
+	JOIN 
+	    courses AS co
+	ON 
+	    a.id = co.from_address_id
+	JOIN 
+	    clients AS cl
+	ON cl.id = co.client_id
+	JOIN 
+	   cars AS c 
+	ON c.id = co.car_id
+	JOIN 
+	   categories AS ca 
+	ON ca.id = c.category_id
 	WHERE 
-		id = account_id;
+	   a.name = address_name
+	ORDER BY c.make, 
+	         cl.full_name;
 END;
 $$
 LANGUAGE plpgsql;
+
+
+
+CALL sp_courses_by_address('66 Thompson Drive');
+
+SELECT * FROM search_results;
